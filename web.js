06792e4ea9f56b330d1eecfee333d203bc1ecebe
Denise Fernandez <dcbfernandez@gmail.com>
2013-07-10 14:09:34 +0000

    modified web.js to now read from index.html

--- web.js
+++ web.js
@@ -2,8 +2,14 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  fs.readFileSync('index.html', function(err, data) {
+      if (*err) throw err;
+      var resp = data.toString();
+      response.send(resp);
+  });
 });
 
 var port = process.env.PORT || 5000;
